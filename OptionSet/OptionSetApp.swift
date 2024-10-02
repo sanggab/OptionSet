cfbbba663026f0dd4ce4f37323b5aa8464a07197
심상갑 <kappa2410@gmail.com>
2024-10-02 09:03:50 +0900

    Initial Commit

---
+++ OptionSet/OptionSetApp.swift
@@ -0,0 +1,17 @@
+//
+//  OptionSetApp.swift
+//  OptionSet
+//
+//  Created by Gab on 2024/10/02.
+//
+
+import SwiftUI
+
+@main
+struct OptionSetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
