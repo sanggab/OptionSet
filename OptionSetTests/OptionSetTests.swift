f43021b978694a08b349f922bfea7fed3c6e7984
심상갑 <kappa2410@gmail.com>
2024-10-08 09:48:52 +0900

    Feat: OptionSet Test 모델 추가

--- OptionSetTests/OptionSetTests.swift
+++ OptionSetTests/OptionSetTests.swift
@@ -29,7 +29,7 @@ final class OptionSetTests: XCTestCase {
     }
     
     func testOneStep() throws {
-        exerciseOption.unionAll(.soccer)
+        exerciseOption.formUnion([.soccer, .hockey])
         print(exerciseOption.stringValue)
         print("상갑 logEvent \(#function) exerciseOption: \(exerciseOption)")
         print("상갑 logEvent \(#function) exerciseOption: \(exerciseOption.stringValue)")
@@ -42,6 +42,11 @@ final class OptionSetTests: XCTestCase {
         }
     }
 
+    
+    override func tearDown() {
+        exerciseOption.clearAll()
+    }
+
 }
 
 private enum ExerciseEnum: String {
@@ -51,24 +56,129 @@ private enum ExerciseEnum: String {
 struct ExerciseOptions: OptionSet {
     let rawValue: Int
     
-    static let soccer = ExerciseOptions(rawValue: 1 << 0)
+    static let soccer       = ExerciseOptions(rawValue: 1 << 0)
+    static let baseball     = ExerciseOptions(rawValue: 1 << 1)
+    static let taekwondo    = ExerciseOptions(rawValue: 1 << 2)
+    static let karate       = ExerciseOptions(rawValue: 1 << 3)
+    static let yoga         = ExerciseOptions(rawValue: 1 << 4)
+    static let kickboxing   = ExerciseOptions(rawValue: 1 << 5)
+    static let climbing     = ExerciseOptions(rawValue: 1 << 6)
+    static let running      = ExerciseOptions(rawValue: 1 << 7)
+    static let golf         = ExerciseOptions(rawValue: 1 << 8)
+    static let cycling      = ExerciseOptions(rawValue: 1 << 9)
+    static let ski          = ExerciseOptions(rawValue: 1 << 10)
+    static let kayak        = ExerciseOptions(rawValue: 1 << 11)
+    static let tennis       = ExerciseOptions(rawValue: 1 << 12)
+    static let bowling      = ExerciseOptions(rawValue: 1 << 13)
+    static let tableTennis  = ExerciseOptions(rawValue: 1 << 14)
+    static let basketball   = ExerciseOptions(rawValue: 1 << 15)
+    static let fencing      = ExerciseOptions(rawValue: 1 << 16)
+    static let football     = ExerciseOptions(rawValue: 1 << 17)
+    static let jogging      = ExerciseOptions(rawValue: 1 << 18)
+    static let volleyball   = ExerciseOptions(rawValue: 1 << 19)
+    static let hockey       = ExerciseOptions(rawValue: 1 << 20)
+    static let iceHockey    = ExerciseOptions(rawValue: 1 << 21)
+    static let training     = ExerciseOptions(rawValue: 1 << 22)
+    static let swimming     = ExerciseOptions(rawValue: 1 << 23)
+    static let crossfit     = ExerciseOptions(rawValue: 1 << 24)
+    static let meditaton    = ExerciseOptions(rawValue: 1 << 25)
+    static let sportsDance  = ExerciseOptions(rawValue: 1 << 26)
+    static let horseRiding  = ExerciseOptions(rawValue: 1 << 27)
+    static let skating      = ExerciseOptions(rawValue: 1 << 28)
+    
     
     static let all: ExerciseOptions = [
-        .soccer
+        .soccer,
+        .baseball,
+        .taekwondo,
+        .karate,
+        .yoga,
+        .kickboxing,
+        .climbing,
+        .running,
+        .golf,
+        .cycling,
+        .ski,
+        .kayak,
+        .tennis,
+        .bowling,
+        .tableTennis,
+        .basketball,
+        .fencing,
+        .football,
+        .jogging,
+        .volleyball,
+        .hockey,
+        .iceHockey,
+        .training,
+        .swimming,
+        .crossfit,
+        .meditaton,
+        .sportsDance,
+        .horseRiding,
+        .skating
     ]
     
-    mutating func unionAll(_ option: ExerciseOptions) {
-        self = self.union(option)
+    mutating func clearAll() {
+        self = self.intersection([.all])
     }
 }
 
 extension ExerciseOptions {
     var stringValue: String {
         switch self {
-        case .soccer:
-            return "Soccer"
-        default:
-            return ""
+        case .soccer:       return "Soccer"
+        case .baseball:     return "Baseball"
+        case .taekwondo:    return "Taekwondo"
+        case .karate:       return "Karate"
+        case .yoga:         return "Yoga"
+        case .kickboxing:   return "Kickboxing"
+        case .climbing:     return "Climbing"
+        case .running:      return "Running"
+        case .golf:         return "Golf"
+        case .cycling:      return "Cycling"
+        case .ski:          return "Ski"
+        case .kayak:        return "Kayak"
+        case .tennis:       return "Tennis"
+        case .bowling:      return "Bowling"
+        case .tableTennis:  return "Table Tennis"
+        case .basketball:   return "Basketball"
+        case .fencing:      return "Fencing"
+        case .football:     return "Football"
+        case .jogging:      return "Jogging"
+        case .volleyball:   return "Volleyball"
+        case .hockey:       return "Hockey"
+        case .iceHockey:    return "Ice Hockey"
+        case .training:     return "Training"
+        case .swimming:     return "Swimming"
+        case .crossfit:     return "Crossfit"
+        case .meditaton:    return "Meditaton"
+        case .sportsDance:  return "Sports dance"
+        case .horseRiding:  return "Horse riding"
+        case .skating:      return "Skating"
+        default:            return ""
         }
+    }
+    
+    var description: [String] {
+        
+        return []
+    }
+}
+
+protocol Option: RawRepresentable, Hashable, CaseIterable, CustomStringConvertible {}
+
+enum Topping: String, Option {
+    case kimchi
+    
+    var description: String {
+        return self.rawValue
+    }
+}
+
+extension Set where Element == Topping {
+    static var hi: Set<Topping> {
+        
+        return [.kimchi]
     }
 }
